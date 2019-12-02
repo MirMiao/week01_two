d05b9f8503f2ded9019d3b1dfbf49ed755ceaad2
MirMiao <1251267855@qq.com>
2019-12-02 18:46:29 +0800

    第一次提交

---
+++ app/src/main/java/com/bw/week01_two/view/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.bw.week01_two.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bw.week01_two.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ app/src/main/java/com/bw/week01_two/view/fragment/HomeFragment.java
@@ -0,0 +1,9 @@
+package com.bw.week01_two.view.fragment;
+
+/**
+ * 时间 :2019/12/2  18:28
+ * 作者 :苗恒
+ * 功能 :
+ */
+public class HomeFragment {
+}
